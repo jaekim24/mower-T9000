7ba3fa9c06232671e7f06abdc0b365f67bb3f7a8
jaekim24 <62858192+jaekim24@users.noreply.github.com>
2021-06-08 16:20:36 -0400

    Update slave.ino

--- I2C TEST/slave.ino
+++ I2C TEST/slave.ino
@@ -1,7 +1,15 @@
 #include <Wire.h>
 #define SLAVE_ADDR 9
 
-//this is a slave
+/*
+-this is a slave
+-slave non tx side GND connect to 
+ master tx side GND
+-A4 and A5 connects to each other for master and slave 
+
+
+*/
+
 
 void setup(){
   Wire.begin(SLAVE_ADDR);
