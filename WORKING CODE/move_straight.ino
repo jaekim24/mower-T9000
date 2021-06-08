781471b27652f4449bcb68e23f96410c3f9188ac
jaekim24 <62858192+jaekim24@users.noreply.github.com>
2021-06-08 18:25:03 -0400

    Update move_straight.ino

--- WORKING CODE/move_straight.ino
+++ WORKING CODE/move_straight.ino
@@ -1,16 +1,16 @@
 #include <Wire.h>
-
-//if motor doesnt adjust, try tx port 
 #include <SoftwareSerial.h>
 #include <SabertoothSimplified.h>
 SoftwareSerial SWSerial(NOT_A_PIN, 12); // RX on no pin (unused), TX on pin 11 (to S1).
 SabertoothSimplified ST(SWSerial); // Use SWSerial as the serial port.
 
 #define SLAVE_ADDR 9
-
 byte x ; 
 
+
 //this is a slave
+//motor 2 is faster than motor 1; motor2 +6 motor1 +0 == same speed 
+
 
 void setup(){
   Wire.begin(SLAVE_ADDR);
