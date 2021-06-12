468c5705b18fe783f68bd87237e6db5556b09248
jaekim24 <62858192+jaekim24@users.noreply.github.com>
2021-06-12 19:58:47 -0400

    program to go to xy

--- go_to_xy.ino
+++ go_to_xy.ino
@@ -16,41 +16,54 @@ distance formula : d = sqrt((x2-x1)^2 + (y2-y1)^2)
 */
 
 int x_target,y_target,x_now,y_now,z_now,z_target;
+float new_z;
 
-int find_distance_of_2_coordinates(int x1,int x2,int y1,int y2){
+
+int find_distance_of_2_coordinates(int x1,int y1,int x2,int y2){
     int distance ;
-
     distance = sqrt((x2-x1)^2 + (y2-y1)^2);
-
     return distance;
 }
 
-float find_angle(){
-    //for right now insertting the target coordinates
 
+float find_angle(int x1,int y1,int x2,int y2,int x3,int y3 ){
+    //                                      (x2,y2) <----target
+    // so the angle is at (x1,y1)           (x3,y3)
     float angle;
-
-    angle = acos(find_distance_of_2_coordinates(0,10,0,0)/find_distance_of_2_coordinates(0,10,0,5));
-
+    angle = acos(find_distance_of_2_coordinates(x1,y1,x2,y2)/find_distance_of_2_coordinates(x1,y1,x3,y3));
     return angle;
 }
 
-void go_to_xy(){
-    // mower is at (0,0) and target is (10,5)
-    x_now = 0;
-    y_now = 0;
-    x_target = 10;
-    y_target = 5;
 
-    //how to you want to set the z as 
-    if(z_now >z_target){
-
-    }    
-
-
+float get_z(){
+    new_z = z - 256; 
+    return (new_z);
 }
 
-void main(){
+
+void go_to_xy(int x1,int y1,int x2,int y2,int x3,int y3){
+    // mower is at (0,0) and target is (10,5)
+    x_now = 0;  // modify slave nano to receive the xy  
+    y_now = 0;  // same here ^
+    x_target = x2;
+    y_target = y2;
+    z_target = find_angle( x1, y1, x2, y2, x3,y3);
+
+    //how to you want to set the z as 
+    while (z_target != get_z()){
+        if(get_z() >z_target){
+            while(get_z()> z_target){
+                ST.motor(1,60); //turns right, moving the left motor
+            }
+            ST.motor(1,0); //stops once z_now == z_target
+        }    
+        if(get_z() < z_target){
+            while(get_z() < z_target){
+                ST.motor(2,60); // turns left, moving the right motor
+            }
+            ST.motor(2,0);
+        }
+    }
 }
 
 
