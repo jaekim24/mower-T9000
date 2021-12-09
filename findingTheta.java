c0a25d39061484d42e6d86c4f7adc02b4a9a0029
jaekim24 <62858192+jaekim24@users.noreply.github.com>
2021-12-08 22:53:27 -0500

    Update findingTheta.java

--- findingTheta.java
+++ findingTheta.java
@@ -51,7 +51,7 @@ static float findingTheta(float x1,float y1,float x2,float y2){
     findingThetaV2 a = new findingThetaV2 (x1,y1);
     findingThetaV2 b = new findingThetaV2 (x2,y2);
 
-    float theta = (float)acos(a.dot(b)/(a.mag(b)+b.mag(a)));
+    float theta = (float)Math.acos(a.dot(b)/(a.mag(b)+b.mag(a)));
 
     return theta; 
 
