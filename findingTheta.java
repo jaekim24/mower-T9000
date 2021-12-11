24dab8987b2756a85007da0c72073f7245235493
jaekim24 <62858192+jaekim24@users.noreply.github.com>
2021-12-10 21:17:46 -0500

    fixed issue

--- findingTheta.java
+++ findingTheta.java
@@ -1,65 +1,39 @@
-import java.lang.*;
-class findingThetaV2{
+class findingTheta{
     float Ax,Ay,Bx,By;
     float x, y;
 
-    findingThetaV2(float x_,float y_){
+    findingTheta(float x_,float y_){
         x = x_;
         y = y_;
     }
 
-        float mag(findingThetaV2 v){
-            return (float) Math.sqrt(x*v.x + y*v.y);
+        float mag(){
+            return (float) Math.sqrt((x*x) +( y*y));
         }
 
-        float dot(findingThetaV2 c){
-            return x*c.x + y*c.y;
+        float dot(findingTheta c){
+            return (x*c.x) + (y*c.y);
         }
 
-/*
-        float getAx(){
-            return Ax_;
+        void debug(findingTheta m){
+            System.out.println( x + ","+ y + ","+ m.x+","+m.y);
         }
-        float getAy(){
-            return Ay_;
-        }
-        float getBx(){
-            return Bx_;
-        }
-        float getBy(){
-            return By_;
-        }
-        
-        void setAx(float x){
-            Ax = x ; 
-        }
-        void setAy(float y){
-            Ay = y;
-        }
-        void setBx(float x){
-            Bx = x;
-        }
-        void setBy(float y){
-            By = y;
-        }
-*/
+
 float dot(float AX,float AY,float BX,float BY){
     return ((AX*BX)+(AY*BY));
 } 
 
-static float findingTheta(float x1,float y1,float x2,float y2){
-    findingThetaV2 a = new findingThetaV2 (x1,y1);
-    findingThetaV2 b = new findingThetaV2 (x2,y2);
-
-    float theta = (float)Math.acos(a.dot(b)/(a.mag(b)+b.mag(a)));
+static float findingThetaMethod(float x1,float y1,float x2,float y2){
+    findingTheta a = new findingTheta (x1,y1);
+    findingTheta b = new findingTheta (x2,y2);
+    float equation =(float)Math.acos(a.dot(b)/(a.mag()*b.mag())); 
+    float theta = (float)Math.toDegrees(equation);
 
     return theta; 
 
 }
 public static void main(String[]arg){
-     findingTheta(10,2,4,-3);
-   
+    System.out.println(findingThetaMethod(4,8,8,13));
 }
 }
-
 
